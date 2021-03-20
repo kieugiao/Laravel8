77affc97ee8f3d91db4ef1dbe2c0a648a0ad178a
ADMIN\Administrator <kieugiao@vutatech.vn>
2021-03-20 13:18:21 +0700

    Authentication

---
+++ app/Http/Controllers/FlightController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FlightController extends Controller
+{
+    /**
+     * Update the flight information for an existing flight.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request)
+    {
+        // $request->user()
+    }
+}
