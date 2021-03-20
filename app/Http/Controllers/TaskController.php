feb05f8607e361fd6d0a8e1e1804294a9c09245f
ADMIN\Administrator <kieugiao@vutatech.vn>
2021-03-20 14:44:36 +0700

    Deleting The Task

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -42,4 +42,13 @@ class TaskController extends Controller
     
         return redirect('/tasks');
     }
+
+    public function destroy(Request $request, Task $task)
+    {
+        $this->authorize('destroy', $task);
+
+        $task->delete();
+
+        return redirect('/tasks');
+    }
 }
