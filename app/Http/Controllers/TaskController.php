2cbf4180f3f4487794e7648618eb124f9c2826ba
ADMIN\Administrator <kieugiao@vutatech.vn>
2021-03-20 14:11:24 +0700

    Dependency Injection

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,21 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Task;
+use App\Http\Requests;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Repositories\TaskRepository;
 
 class TaskController extends Controller
 {
+    protected $tasks;
+
     public function __construct()
     {
         $this->middleware('auth');
+        $this->tasks = $tasks;
     }
 
     public function index(Request $request)
     {
-        $tasks = $request->user()->tasks()->get();
+        /*$tasks = $request->user()->tasks()->get();
 
         return view('tasks.index', [
             'tasks' => $tasks,
+        ]);*/
+        return view('tasks.index', [
+            'tasks' => $this->tasks->forUser($request->user()),
         ]);
     }
 
